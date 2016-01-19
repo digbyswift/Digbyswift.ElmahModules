78fdb2cb6f516cf07f6e78e07de0d1cf3eb6ec85
Kieron McIntyre <kieron.mcintyre@digbyswift.com>
2016-01-19 11:53:37 +0000

    Committal of initial files including AirbrakeErrorLogModule and a basic test MVC application

---
+++ src/Digbyswift.ElmahModules/AirbrakeErrorLogModule.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Diagnostics;
+using System.Web;
+using Elmah;
+using SharpBrake;
+
+namespace Digbyswift.ElmahModules
+{
+
+    public class AirbrakeErrorLogModule : ErrorLogModule
+    {
+
+        protected override void LogException(Exception e, HttpContext context)
+        {
+            if (e == null)
+                throw new ArgumentNullException("e");
+
+            var args = new ExceptionFilterEventArgs(e, context);
+            OnFiltering(args);
+
+            if (args.Dismissed)
+                return;
+
+            try
+            {
+                e.SendToAirbrake();
+            }
+            catch (Exception localException)
+            {
+                Trace.WriteLine(localException);
+            }
+        }
+
+    }
+
+}
---
+++ src/TestSite/Controllers/HomeController.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Web.Mvc;
+
+namespace TestSite.Controllers
+{
+    public class HomeController : Controller
+    {
+        public ActionResult Index()
+        {
+            throw new InvalidOperationException("Intentional exception throw");
+        }
+
+    }
+}
